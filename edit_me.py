0c85f069586d962687ebdb122180d1490df6b8ce
ESimental2002 <esimental@drury.edu>
2025-03-17 17:16:54 -0500

    ezpz work, even thought it took me forever and honestly didnt realize we had this.

--- edit_me.py
+++ edit_me.py
@@ -1,11 +1,23 @@
 
 def monotonic(lst):
-    """Return True if lst is monotonic; return False, otherwise."""
-    #
-    # YOUR IMPLEMENTATION GOES HERE
-    #
+    if len(lst) <= 1:
+        return True
 
-#
-# Feel free to replace these comments with
-# code that tests your function monotonic.
-#
+    is_ascending = True
+    is_descending = True
+
+    for i in range(1, len(lst)):
+        if lst[i] < lst[i - 1]:
+            is_ascending = False
+        if lst[i] > lst[i - 1]:
+            is_descending = False
+
+    return is_ascending or is_descending
+
+print(monotonic([12, 16, 34, 80])) 
+print(monotonic([45, 12, -10, -19]))  
+print(monotonic([2, 3, 2, 3]))  
+print(monotonic([5, 5, 5]))  
+print(monotonic([]))  
+print(monotonic([1405006]))  
+print(monotonic([100, 200, 150, 250, 300]))
